4d8217ba1d7426b99e064129715fe85773220fc7
dgladovich <dgladovich@gmail.com>
2018-06-19 06:42:19 +0300

    adding nessary files;

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,4 +1,9 @@
+import * as types from '../constants/ActionTypes';
+
+let nextMessageId = 0;
+let nextUserId = 0;
 const URL = 'http://localhost:5000/controllers';
+
 export const fetchControllers = ()=> (dispatch, getState) => {
     return fetch(URL)
         .then((data)=>{
@@ -13,3 +18,29 @@ export const fetchControllers = ()=> (dispatch, getState) => {
 export const toggleTodo = () => (dispatch, getState) => {
   console.log('Toggling todo')
 };
+
+
+export const addMessage = (message, author) => ({
+    type: types.ADD_MESSAGE,
+    id: nextMessageId++,
+    message,
+    author
+});
+
+export const addUser = name => ({
+    type: types.ADD_USER,
+    id: nextUserId++,
+    name
+});
+
+export const messageReceived = (message, author) => ({
+    type: types.MESSAGE_RECEIVED,
+    id: nextMessageId++,
+    message,
+    author
+});
+
+export const populateUsersList = users => ({
+    type: types.USERS_LIST,
+    users
+});
